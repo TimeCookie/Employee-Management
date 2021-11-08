7595ea6ffaff6d216e72cb37ca4c1bdb820a5009
Andre Jonathan <andrejonathan116@gmail.com>
2021-11-08 20:13:50 +0700

    Creating New login user and admin option

---
+++ pages/admin-login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Admin</title>
+    <link rel="stylesheet" href="../assets/css/admin-login.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="cont">
+        <div class="form sign-in">
+            <h2>Admin Sign In</h2>
+            <label>
+                <span>Username</span>
+                <input type="Username" name="username">
+            </label>
+            <label>
+                <span>Password</span>
+                <input type="password" name="password">
+            </label>
+            <button class="submit" type="button">Log in</button>
+        </div>
+        <div class="sub-cont">
+            <div class="img">
+                <div class="img-text m-in">
+                    <h2>User</h2>
+                    <p>LAI MASUK User</p>
+                    <div class="container">
+                        <a href="../pages/user-login.php">
+                            <button class="btn btn1">Sign In</button>
+                        </a> 
+                    </div>
+                </div>
+                
+        </div>
+    </div>
+    <script type="text/javascript" src="script.js"></script>
+</body>
+</html>
---
+++ pages/user-login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../assets/css/user-login.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="cont">
+        <div class="form sign-in">
+            <h2>User Sign In</h2>
+            <label>
+                <span>Username</span>
+                <input type="Username" name="username">
+            </label>
+            <label>
+                <span>Password</span>
+                <input type="password" name="password">
+            </label>
+            <button class="submit" type="button">Log in</button>
+        </div>
+        <div class="sub-cont">
+            <div class="img">
+                <div class="img-text m-in">
+                    <h2>Admin</h2>
+                    <p>LAI MASUK ADMIN</p>
+                </div>
+            <div class="container">
+                <a href="../pages/admin-login.php">
+                    <button class="btn btn1">Sign In</button>
+                </a> 
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>

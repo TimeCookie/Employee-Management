f18b926407811d24c82066d70c2ffb187eeda17a
Bryan Tandian <bryandimana@gmail.com>
2021-11-12 19:46:57 +0700

    Department Search Feature Done

--- pages/admin/department.php
+++ pages/admin/department.php
@@ -1,11 +1,20 @@
+<?php
+include '../../modules/db_connect.php';
+//ketika ditekan
+
+/*else {
+echo 'The result is empty';
+}*/
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Department</title>
-    <link rel="stylesheet" href="../../assets/css/department.css">
+    <title>employee</title>
+    <link rel="stylesheet" href="../../assets/css/employee.css">
     <!-----ini Box icon ------>
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
@@ -21,7 +30,7 @@
         </div>
         <ul class="nav_list">
             <li>
-                <a href="dashboard.php">
+                <a href="admin-dashboard.php">
                     <i class='bx bx-home'></i>
                     <span class="link_name">Home</span>
                 </a>
@@ -33,13 +42,6 @@
                     <span class="link_name">User</span>
                 </a>
                <span class="tooltip">User</span>
-            </li>
-            <li>
-                <a href="department.php">
-                    <i class='bx bxs-school'></i>
-                    <span class="link_name">Department</span>
-                </a>
-               <span class="tooltip">Department</span>
             </li>
             <li>
                 <a href="add-task.php">
@@ -56,7 +58,7 @@
                <span class="tooltip">Setting</span>
             </li>
             <li>
-                <a href="../modules/logout.php">
+                <a href="../../modules/logout.php">
                     <i class='bx bx-power-off'></i>
                     <span class="link_name">Log out</span>
                 </a>
@@ -80,39 +82,69 @@
     </div>
 
     <div class="add_content">
-        <div class="text">Department</div>
+        <div class="text">Employees</div>
         
     </div> 
 
     <!-----------------Search Bar ADD EMPLOYEE----------->
     <div class="container">
         <div class="search-box">
-            <input type="text" class="search" placeholder="what are you looking for?"/>
-            <button type="submit" class="search-btn">
-                <i class='bx bx-search-alt-2'></i>
+        <form action="department.php" method="POST">
+            <input type="text" class="search" name="typing" placeholder="what are you looking for?"/>
+            <button type="submit" name="searching" class="search-btn" > 
+                <i class='bx bx-search'></i>
             </button>
+
+        </form>    
         </div>
     </div>
+    
 
 
     <!------------Button Add Employe----------->
-    <a href="edit-department.php" class="button1">Add Department </a>
+    <a href="#" class="button1">Add Department</a>
 
 
-    <!---------Container Employee------------->
+    <!---------Container Department------------->
     <div class="employee-container">
         <div class="row"> <!--Container-->
-            <div class="col-lg-3 col-sm-12">
+        <?php
+        
+        if(isset($_POST["searching"])) {
+
+            $searchRes = mysqli_real_escape_string($con, $_POST['typing']);
+            if($searchRes!='') {
+
+            $query ="SELECT department_name,department_id FROM department dep WHERE dep.department_name LIKE '%$searchRes%'";
+
+            $result = mysqli_query($con,$query);
+   
+    
+            if(mysqli_num_rows($result) > 0){
+                while($rows = mysqli_fetch_assoc($result)) { 
+                                 
+        ?>
+            <div class="col-lg-4 col-sm-12">
                 <div class="jumbotron card2">
                     <div class="image1">
-                        <img src="../assets/img/test1.jpg" alt="people" class="img-fluid rounded-circle  image2">
+                        <img src="../../assets/img/test1.jpg" class="img-fluid rounded-circle  image2">
                     </div>
                     <div class="info">
-                        <h4>Department Name </h4>
-                        <p>Desc</p>
-                        <a href="edit-department.php" class="button3">Edit</a>
+                        <h4><?php echo $rows['department_name'];?> </h4>
+                        <p> <?php echo $rows['department_id'];?> </p>
+                        <a href="#" class="button3">Edit Profile</a>
                     </div>
                 </div>
+            </div>
+        <?php
+                    }
+                }
+                else {
+                    echo " ";
+                }
+            }
+        }
+        ?>
               <!-- Code goes here -->
             </div>        
 
